91f5033fa1c4a3b107c2b5b51861ef0faee3ee0d
Jacob Jonkman <jacob.jonkman@gmail.com>
2017-12-18 16:59:20 +0100

    mutation en deel selection af

--- PA2/dekkers_jonkman_es.m
+++ PA2/dekkers_jonkman_es.m
@@ -1,9 +1,71 @@
 function [xopt,fopt] = dekkers_jonkman_es(eval_budget)
-    gen = 0;
-    thickness = 30;
-    pop_size = 100;
-    pop = zeros(pop_size, thickness); % declare pop_size solution vectors of length n
-	population = randi([0,10000], pop_size, thickness) % randomly initialize solution i
-    optical(population(1,:))
+    max_thickness = 10000;
+    lambda = 100;
+    layers = 30;
+    pop_size = 50;
+    recomb_rate = 0.75;
+    mut_rate = 0.25;
+    tau = 1/(sqrt(2*sqrt(layers)));
+    tauprime = 1/(sqrt(2*layers));
     
+    pop = rand(pop_size,layers).*max_thickness; % randomly initialize population
+    sigmas = zeros(pop_size,layers); % initialize individual sigmas
+    sigmas(1:pop_size,1:layers) = max_thickness/4;
+    %fitnesses = zeros(1,lambda);
+    
+    %for i=1:pop_size
+    %    i
+    %    fitnesses(:,1) = optical(pop(i,:)); % initialize population fitnesses
+    %end
+    
+    for i = 1:eval_budget/lambda
+        [offspring, newsigmas] = recombine(pop, sigmas, lambda, pop_size, layers, recomb_rate);
+        [offspring, newsigmas] = mutate(offspring, newsigmas, lambda, mut_rate, tau, tauprime);
+        fitnesses = zeros(1, lambda);
+        for j = 1:lambda
+            fitnesses(1,j) = str2num(optical(offspring(j,:)));
+        end
+        [pop,sigmas] = select(offspring, fitnesses, newsigmas, pop_size, layers);
+        min(fitnesses)
+    end
+end
+
+function [pop, sigmas] = select(offspring, fitnesses, newsigmas, pop_size, layers)
+    pop = zeros(pop_size, layers);
+    sigmas = zeros(pop_size, layers);
+    %Pak daadwerkelijk pop_size kinderen ipv ze allemaal
+    [~,idx] = sort(fitnesses);
+    pop = offspring(idx);
+    sigmas = newsigmas(idx);
+    %
+end
+
+function [offspring, newsigmas] = mutate(offspring, newsigmas, lambda, mut_rate, tau, tauprime)
+    for i=1:lambda
+        if rand() < mut_rate
+            newsigmas(i,:) = newsigmas(i,:)*exp(tauprime*normrnd(0,1) + tau*normrnd(0,1));
+            offspring(i,:) = offspring(i,:) + newsigmas(i,:) * normrnd(0,1);
+        end
+    end
+end
+
+function [offspring, newsigmas] = recombine(pop, sigmas, lambda, pop_size, layers, recomb_rate)
+    offspring = zeros(lambda, layers);
+    newsigmas = zeros(lambda, layers);
+    for i=1:lambda
+        par1 = randi([1,pop_size]);
+        par2 = randi([1,pop_size]);
+        while par1==par2
+            par2 = randi([1,pop_size]);
+        end
+        for j=1:layers
+            if rand() <= recomb_rate
+                offspring(i,j) = pop(par1,j);
+                newsigmas(i,j) = sigmas(par1,j);
+            else
+                offspring(i,j) = pop(par2,j);
+                newsigmas(i,j) = sigmas(par2,j);
+            end
+        end
+    end
 end
